b31bae21fe5ace1306fbf90b837141f2cf5ac5f6
guobao.jiang <loveaborn@foxmail.com>
2016-10-14 20:09:24 +0800

    sync export

--- cookbook.el
+++ cookbook.el
@@ -9,6 +9,18 @@
                (and (s-ends-with? ".org" x)
                     (not (s-ends-with? "README.org" x))))
            (f-files cookbook-root-dir))))
-    (setq ab/debug files)))
+    (mapc #'(lambda (x)
+              (let* ((src-file x)
+                     (dest-file (concat
+                                 (substring x 0 (- (length x) 3))
+                                 "pdf")))
+                (cookbook-org-to-pdf src-file dest-file)))
+          files)))
+
+(defun cookbook-org-to-pdf ()
+  "export source file to dest files"
+  ;;(message "src-file: %s dest-file:%s" src-file dest-file)
+  (find-file "/Users/aborn/github/emacs-cookbook/org.org")
+  (org-latex-export-to-pdf))
 
 (provide 'cookbook)
