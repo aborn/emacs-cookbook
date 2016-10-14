03c3a58da29be179449600d5ab9789d39f441062
Aborn Jiang <loveaborn@foxmail.com>
2016-10-15 00:46:19 +0800

    add GPL 3

--- cookbook.el
+++ cookbook.el
@@ -1,5 +1,34 @@
 ;;; cookbook.el --- produce cookbook pdf  -*- lexical-binding: t; -*-
 
+;; Copyright (C) 2016  Aborn Jiang
+
+;; Author: Aborn Jiang <aborn.jiang@gmail.com>
+;; Version: 0.1.0
+;; Keywords: emacs cookbook
+;; URL: https://github.com/aborn/emacs-cookbook
+
+;; This file is NOT part of GNU Emacs.
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; The package to build cookbook
+;;
+
+;;; Code:
+
 (defvar cookbook-root-dir "~/github/emacs-cookbook/")
 
 (defun cookbook-org-async-batch-export-to-pdf ()
